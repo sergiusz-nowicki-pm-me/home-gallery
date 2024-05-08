4b74e3d38a58dccf8634c13ab4656761504cb3f0
Sergiusz Nowicki <sergiusz.nowicki@p.lodz.pl>
2024-05-08 17:31:08 +0200

    .

--- pocol-back/src/rest_api/start.py
+++ pocol-back/src/rest_api/start.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 import rest_api.config_endpoints
 import rest_api.query_endpoints
 import rest_api.catalogue_endpoints
+import rest_api.images_endpoints
 
 app = Flask(__name__)
 CORS(app)
@@ -15,3 +16,4 @@ def index():
 rest_api.config_endpoints.prepare(app)
 rest_api.query_endpoints.prepare(app)
 rest_api.catalogue_endpoints.prepare(app)
+rest_api.images_endpoints.prepare(app)
